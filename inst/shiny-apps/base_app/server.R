7e9927ea5d4224b260344504229e83c87dccf604
Richard Schuster <ricschuster@users.noreply.github.com>
2018-01-30 14:02:41 +0100

    solve

--- inst/shiny-apps/base_app/server.R
+++ inst/shiny-apps/base_app/server.R
@@ -26,6 +26,67 @@ function(input, output, session) {
     
     return (pu)
   })
+  
+
+  prob <- eventReactive(input$Bproblem, {
+    pu <- pu()
+    
+    p <- problem(pu, features = input$feat_col, cost_column = input$cost_col)
+    
+    return(p)
+  })
+  
+  solve <- eventReactive(input$Bsolve, {
+    
+    p <- prob() 
+    
+    #add objective  
+    if (input$objective == "min_set"){
+      p <- p %>% add_min_set_objective()
+      
+    } else if (input$objective == "max_cov"){
+      p <- p %>% add_max_cover_objective(input$budget)
+      
+    } else if (input$objective == "max_feat"){
+      p <- p %>% add_max_features_objective(input$budget)
+      
+    } else if (input$objective == "max_phylo"){
+      p <- p %>% add_max_phylo_objective(input$budget, input$phylo)
+      
+    } else if (input$objective == "max_util"){
+      p <- p %>% add_max_utility_objective(input$budget)
+
+    } else{
+      #Throw error or show warning
+    }
+    
+    
+    #add targets
+    if (input$objective %in% c("min_set", "max_feat", "max_phylo")){
+      
+      if(input$glob_tar == "global"){
+        tmp_tar <- input$tar_all
+      } else {
+        tmp_tar <- 1 #get target values from rhandsontable
+      }
+      
+      if(input$tar_type == "rel_tar"){
+        p <- p %>% add_relative_targets(tmp_tar)
+        
+      } else if(input$tar_type == "abs_tar"){
+        p <- p %>% add_relative_targets(tmp_tar)
+        
+      } else if(input$tar_type == "log_tar"){
+        p <- p %>% add_loglinear_targets(tmp_tar)
+        
+      }
+    }
+    
+    s <- solve(p)
+    
+    return(s)
+    
+  })
 
   
   #################################################################################################################
@@ -111,11 +172,13 @@ function(input, output, session) {
   #End shinyjs checks
   #################################################################################################################
   
-  output$contents <- renderTable({
+  output$contents <- renderPrint({
     
-    req(input$x)
-    out <- pu()@data
-    return(out)
+    #req(input$x)
+    pp <- prob()
+    
+    print(pp)
+    
   })
   
   
