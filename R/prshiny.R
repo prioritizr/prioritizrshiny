73a1a58966128f3fb687dd07bb2e3a6575005be3
RicSchuster <mail@richard-schuster.com>
2019-07-01 10:26:33 -0700

    prshiny as app

--- R/prshiny.R
+++ R/prshiny.R
@@ -28,26 +28,26 @@ NULL
 NULL
 
 #' @export
-prshiny <- function(app, ...) {
-  # locate all the shiny apps that exist
-  validapps <- list.files(system.file("shiny-apps", package = "prioritizrshiny"))
-
-  validappsMsg <-
-    paste0(
-      "Valid apps are: '",
-      paste(validapps, collapse = "', '"),
-      "'")
-
-  # if an invalid app is given, throw an error
-  if (missing(app) || !nzchar(app) ||
-      !app %in% validapps) {
-    stop(
-      'Please run `prshiny()` with a valid app as an argument.\n',
-      validappsMsg,
-      call. = FALSE)
-  }
-
-  # find and launch the app
-  appDir <- system.file("shiny-apps", app, package = "prioritizrshiny")
+prshiny <- function(...) {
+  # # locate all the shiny apps that exist
+  # validapps <- list.files(system.file("shiny-apps", package = "prioritizrshiny"))
+  # 
+  # validappsMsg <-
+  #   paste0(
+  #     "Valid apps are: '",
+  #     paste(validapps, collapse = "', '"),
+  #     "'")
+  # 
+  # # if an invalid app is given, throw an error
+  # if (missing(app) || !nzchar(app) ||
+  #     !app %in% validapps) {
+  #   stop(
+  #     'Please run `prshiny()` with a valid app as an argument.\n',
+  #     validappsMsg,
+  #     call. = FALSE)
+  # }
+  # 
+  # # find and launch the app
+  appDir <- system.file("shiny-apps", "prshiny", package = "prioritizrshiny")
   shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE, ...)
 }
